205d00fab24b24ce4e31ccef2a5b50468c133b92
Beaast-exe <r.fariaa18@gmail.com>
2024-01-09 12:49:23 +0000

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import './navbar.css';
+import { FaWhatsapp, FaInstagram, FaYoutube, FaFacebook } from 'react-icons/fa';
 
 const Navbar = () => {
 	return (
 		<div className="nav-container">
-			
+			<div className="nav-content">
+				<h1>CarShop</h1>
+
+				<ul>
+					<li>Home</li>
+					<li>Buy</li>
+					<li>Sell</li>
+					<li>Stock</li>
+					<li>Contact</li>
+				</ul>
+
+				<div className="nav-contact">
+					<FaWhatsapp />
+					<span>919 999 999</span>
+				</div>
+
+				<div className="nav-social">
+					<FaInstagram />
+					<FaYoutube />
+					<FaFacebook />
+				</div>
+			</div>
 		</div>
 	)
 }
